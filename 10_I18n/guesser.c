f8178f26da924344805607a7c4375987474a87d9
Egor Zinkin <pifagor36@yandex.ru>
2021-12-19 16:41:50 +0300

    Update 1

--- 10_I18n/guesser.c
+++ 10_I18n/guesser.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <libgen.h>
 #include <libintl.h>
 #include <locale.h>
 
 #define _(STRING) gettext(STRING)
-#define LOCALE_PATH "."
 
 int guess(int a, int b) {
 	if (a == b) {
@@ -16,7 +13,7 @@ int guess(int a, int b) {
 	printf(_("Is your number less then %d?\n"), half);
 	char answer[64];
 	scanf("%s", answer);
-	if (strcmp(answer, "no") == 0) {
+	if (strcmp(answer, _("no")) == 0) {
 		return guess(half, b);
 	} else {
 		return guess(a, half - 1);
@@ -25,12 +22,14 @@ int guess(int a, int b) {
 
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	/*
 	char *dir;
-
 	dir = dirname(realpath(argv[0], NULL));
+	*/
+
 	setlocale (LC_ALL, "");
-	bindtextdomain ("guesser", LOCALE_PATH);
+	bindtextdomain ("guesser", "po");
 	textdomain ("guesser");
 
 	printf(_("I will try to guess a number from 1 to 100\n"));
